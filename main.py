99ac70b2b3a35208e5eb83c61da31f4910318dc1
wessam mohamed <chaos5starz@gmail.com>
2026-03-23 17:34:48 +0200

    added correct number of pages to each topic

--- main.py
+++ main.py
@@ -6,14 +6,13 @@ pdf = FPDF(orientation='p' , unit = 'mm' , format = 'A4')
 df = pd.read_csv("topics (2).csv")
 
 for index , row in df.iterrows() :
-    pdf.add_page()
-    pdf.set_font(family="Times", style='B', size=24)
-    pdf.set_text_color(100 , 100 , 100)
-    pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1)
-    pdf.line(10,22,200,22)
 
-
-
+    for i in range(int(row["Pages"])) :
+        pdf.add_page()
+        pdf.set_font(family="Times", style='B', size=24)
+        pdf.set_text_color(100 , 100 , 100)
+        pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1)
+        pdf.line(10,22,200,22)
 
 
 pdf.output('test.pdf', 'F')
